ef92d63c8b8075ba8b74ba6920f2540dae262558
Vivekanand Rao <vivekanand.rao@gmail.com>
2016-07-30 19:36:07 -0700

    Defined User Registration Routes.

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,31 @@ var express = require('express');
 var router = express.Router();
 
 // GET /
+// the Home Page
 router.get('/', function(req, res, next){
 	return res.render('index', {title: 'Home'});
 });
 
 // GET /about
+// the About Us Page
 router.get('/about', function(req, res, next){
 	return res.render('about', {title: 'About'});
 });
 
 // GET /contact
+// the Contact Us Page
 router.get('/contact', function(req, res, next){
 	return res.render('contact', {title: 'Contact Us'});
+});
+
+//  GET /register
+router.get('/register', function(req, res, next){
+	return res.send('Register Today!!!');
+});
+
+// POST /register
+router.post('/register', function(req, res, next){
+	return res.send('User Created.');
 });
 
 module.exports = router;
