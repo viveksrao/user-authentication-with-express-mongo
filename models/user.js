2cea99f5d03e89f740ea383a29e53d574f7ed958
Vivekanand Rao <vivekanand.rao@gmail.com>
2016-08-05 10:21:46 -0700

    Added Logic to compare the credentials a user supplies in the login form with user credentials in the Mongo database.

--- models/user.js
+++ models/user.js
@@ -24,6 +24,26 @@ var UserSchema = new mongoose.Schema({
 	}
 });
 
+// authenticate input against database documents
+UserSchema.statics.authenticate = function(email, password, callback){
+	User.findOne({email: email}).exec(function(error, user){
+		if(error){
+			return callback(error);
+		}else if(!user){
+			var err = new Error('User not found.');
+			err.status = 401;
+			return callback(err);
+		}
+		bcrypt.compare(password, user.password, function(error, result){
+			if(result === true){
+				return callback(null, user);
+			}else{
+				result callback();
+			}
+		});
+	});
+}
+
 // hash password before saving to database
 UserSchema.pre('save', function(next){
 	var user = this;
